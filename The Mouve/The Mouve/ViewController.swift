a96bdbaa49ad59f2e52e4cd69b3c8476f1ca3e5c
Andrew Breckenridge <asbreckenridge@me.com>
2015-04-12 16:54:57 -0500

    create and add parse

---
+++ The Mouve/The Mouve/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  The Mouve
+//
+//  Created by Andrew Breckenridge on 4/12/15.
+//  Copyright (c) 2015 Andrew Breckenridge. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
