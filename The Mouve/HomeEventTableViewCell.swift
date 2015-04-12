1d0c9369d3f14bc534b194197914d86aa5afff75
Andrew Breckenridge <asbreckenridge@me.com>
2015-04-12 16:50:40 -0500

    pre new swift 1.2 branch

---
+++ The Mouve/HomeEventTableViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  HomeEventTableViewCell.swift
+//  The Mouve
+//
+//  Created by Andrew Breckenridge on 4/8/15.
+//  Copyright (c) 2015 Andrew Breckenridge. All rights reserved.
+//
+
+import UIKit
+
+class HomeEventTableViewCell: UITableViewCell {
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        
+    }
+}
