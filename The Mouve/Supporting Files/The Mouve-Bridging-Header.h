f0e5e5717cdfc0e930ebd4691900cea72c5023d1
Andrew Breckenridge <asbreckenridge@me.com>
2015-04-24 23:14:29 -0500

    add ECSliding + work on storyboard some more

---
+++ The Mouve/Supporting Files/The Mouve-Bridging-Header.h
@@ -0,0 +1,9 @@
+//
+//  The Mouve-Bridging-Header.h
+//  The Mouve
+//
+//  Created by Andrew Breckenridge on 4/24/15.
+//  Copyright (c) 2015 Andrew Breckenridge. All rights reserved.
+//
+
+#import "ECSlidingViewController.h"
