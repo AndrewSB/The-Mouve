5c082d52d1111f408ecc44ec8228e75df1e4df49
Hilal Habashi <habashi2@illillinois.edu>
2015-07-13 18:27:52 -0500

    Added a new file Realm model

---
+++ The Mouve/Models/Users.swift
@@ -0,0 +1,10 @@
+//
+//  Users.swift
+//  The Mouve
+//
+//  Created by Hilal Habashi on 7/13/15.
+//  Copyright (c) 2015 Andrew Breckenridge. All rights reserved.
+//
+
+import Foundation
+import Realm
