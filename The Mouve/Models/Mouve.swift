c097f7a1b5bf64395d37cdec40b42483aa4159d9
YouKnowOJ <ojogbo2@illinois.edu>
2015-07-13 20:36:02 -0500

    Added Mouve model

---
+++ The Mouve/Models/Mouve.swift
@@ -0,0 +1,36 @@
+//
+//  Mouve.swift
+//  The Mouve
+//
+//  Created by Samuel Ifeanyi Ojogbo Jr. on 7/13/15.
+//  Copyright (c) 2015 Andrew Breckenridge. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+
+class Person: Object{
+}
+
+class Mouve: Object {
+    dynamic var owner: Person? // Can be optional
+    
+    dynamic var name = ""
+    dynamic var details = ""
+    //    dynamic var location=
+
+    dynamic var image = ""
+    dynamic var comments = ""
+
+    dynamic var privacy = Bool()
+
+    dynamic var start = NSDate()
+    dynamic var end = NSDate()
+    dynamic var createdAt = NSDate()
+    dynamic var updatedAt = NSDate()
+    
+    let inviteList = List<Person>()
+
+    
+
+}
