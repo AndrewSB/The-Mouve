b5bf2ba9ae45cb4a697837247c0358caf460d097
Hilal Habashi <habashi2@illillinois.edu>
2015-08-21 17:10:18 -0500

    Set a basic activity class with an enum. Waiting on profile page, to implement activities.

--- The Mouve/Activity.swift
+++ The Mouve/Activity.swift
@@ -8,17 +8,40 @@
 
 import Foundation
 import Parse
+enum typeKeyEnum:String {
+    case AddImage = "addimage"
+    case Follow = "follow"
+    case Comment = "comment"
+    case Attend = "attend"
+}
 
-class Activity {
-    @NSManaged var type: String
+class Activity: PFObject {
+    @NSManaged var typeKey: String
     @NSManaged var fromUser: PFUser
     @NSManaged var toUser: PFUser
     @NSManaged var content: String
     @NSManaged var event: Events
+}
+
+extension Activity: PFSubclassing{
+//    setting the type key as enum
+    var type:typeKeyEnum? {
+        
+        get { return self["typeKey"] != nil ? typeKeyEnum(rawValue: self["typeKey"] as! String) : nil }
+        
+        set { return self["typeKey"] = newValue?.rawValue }
+        
+    }
+    //1
+    class func parseClassName() -> String {
+        return "Activty"
+    }
     
-//    // Type values
-//    NSString *const kPAPActivityTypeLike       = @"like";
-//    NSString *const kPAPActivityTypeFollow     = @"follow";
-//    NSString *const kPAPActivityTypeComment    = @"comment";
-//    NSString *const kPAPActivityTypeJoined     = @"joined";
+    //2
+    override class func initialize() {
+        var onceToken: dispatch_once_t = 0
+        dispatch_once(&onceToken) {
+            self.registerSubclass()
+        }
+    }
 }
